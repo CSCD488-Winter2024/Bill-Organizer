9e609d52b2ff75250f35b95a1b702176ddb04c4d
Steven Harper <20556807+stevenharperja@users.noreply.github.com>
2024-05-01 05:49:13 +0000

    add basic query functionality

--- frontend/billorganizer_frontend/bill_app/views.py
+++ frontend/billorganizer_frontend/bill_app/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from django.views.generic import TemplateView, ListView
 from .models import bill
 from django.http import HttpResponse
+from django.db.models import Q 
 
 # Create your views here.
 def index(request):
@@ -21,3 +22,12 @@ class HomePageView(TemplateView):
 class SearchResultsView(ListView):
     model = bill
     template_name = 'search_results.html'
+    # queryset = bill.objects.filter(billname__icontains='2')
+    
+    #override the inherited method
+    def get_queryset(self):
+        query = self.request.GET.get("q")
+        object_list = bill.objects.filter(
+            Q(billname__icontains=query) | Q(text__icontains=query)
+        )
+        return object_list
