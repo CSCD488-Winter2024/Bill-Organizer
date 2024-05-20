644263d0afa7ebfb377db1fe1048b3ac6040b6ec
Gunnar Knutson <gunnar.knutson@proton.me>
2024-05-19 18:35:38 -0700

    update database sql creation file

--- create-db.sql
+++ create-db.sql
@@ -1,27 +1,54 @@
 create table if not exists bills
 (
-    source         varchar(255)                          not null,
-    id             varchar(255)                          not null
-        primary key,
-    status         varchar(255)                          not null,
-    sponsor        varchar(255)                          not null,
-    last_update    timestamp default current_timestamp() not null on update current_timestamp(),
-    title          varchar(255)                          not null,
-    companion_bill varchar(255)                          null,
-    vetoed         binary(1) default '0'                 not null,
-    passing_status binary(1)                             null,
-    info_link      varchar(255)                          not null,
-    party          varchar(255)                          not null,
-    constraint bills_bills_id_fk
-        foreign key (companion_bill) references bills (id)
+    biennium           varchar(255)                                              not null,
+    bill_id            varchar(255)                                              not null,
+    bill_number        smallint unsigned                                         not null,
+    substitute_version tinyint unsigned                                          not null,
+    engrossed_version  tinyint unsigned                                          not null,
+    original_agency    varchar(255)                                              not null,
+    active             boolean                                                   not null,
+    companion_bill     varchar(255)                                              null,
+    fiscal_notes       set('State', 'Local')                                     null,
+    requested_by       set('Governor', 'BudgetCommittee', 'Department', 'Other') null,
+    first_accessed     timestamp default CURRENT_TIMESTAMP                       not null,
+    last_accessed      timestamp default CURRENT_TIMESTAMP                       not null,
+    last_updated       timestamp default CURRENT_TIMESTAMP                       not null,
+    appropriations     boolean                                                   not null,
+    short_description  varchar(255)                                              not null,
+    request            varchar(255)                                              not null,
+    introduced_date    timestamp                                                 not null,
+    sponsor            varchar(255)                                              not null,
+    sponsor_id         smallint unsigned                                         not null,
+    long_description   varchar(255)                                              not null,
+    legal_title        varchar(255)                                              not null,
+    primary key (biennium, bill_id)
 );
 
-create table if not exists users
+create table if not exists status
 (
-    uname varchar(16)  not null
-        primary key,
-    pass  varchar(97)  not null,
-    email varchar(255) null
+    biennium                 varchar(255)                        not null,
+    bill_id                  varchar(255)                        not null,
+    history_line             varchar(255)                        not null,
+    action_date              timestamp                           not null,
+    retrieved                timestamp default CURRENT_TIMESTAMP not null,
+    amended_by_opposite_body boolean                             not null,
+    vetoed                   set('true', 'partial', 'false')     not null,
+    amendments_exist         boolean                             not null,
+    status                   varchar(255)                        not null,
+    foreign key (biennium, bill_id) references bills(biennium, bill_id)
+);
+
+create table if not exists sponsors
+(
+    biennium   varchar(255) not null,
+    party      varchar(255) not null,
+    district   varchar(255) not null,
+    phone      varchar(255) not null,
+    email      varchar(255) not null,
+    first_name varchar(255) not null,
+    last_name  varchar(255) not null,
+    primary key (biennium, email),
+    index (biennium, last_name)
 );
 
 create table if not exists lists
@@ -29,38 +56,33 @@ create table if not exists lists
     id     uuid default uuid() not null
         primary key,
     color  tinyint             null,
-    author varchar(16)         not null,
+    author int(11)             not null,
     name   varchar(255)        not null,
-    constraint lists_users_uname_fk
-        foreign key (author) references users (uname)
-            on delete cascade
+    foreign key (author) references auth_user (id)
+        on delete cascade
 );
 
 create table if not exists marks
 (
-    list uuid         not null,
-    bill varchar(255) not null,
-    primary key (list, bill),
-    constraint marks_bills_id_fk
-        foreign key (bill) references bills (id),
-    constraint marks_lists_id_fk
-        foreign key (list) references lists (id)
-            on delete cascade
+    list uuid             not null,
+    biennium varchar(255) not null,
+    bill_id  varchar(255) not null,
+    foreign key (biennium, bill_id) references bills(biennium, bill_id),
+    foreign key (list) references lists(id)
+        on delete cascade
 );
 
 create table if not exists notes
 (
-    id            uuid      default uuid()              not null
+    id            uuid default uuid()                   not null
         primary key,
     content       text                                  null,
-    author        varchar(255)                          not null,
+    author        int(11)                               not null,
     creation_time timestamp default current_timestamp() not null,
     edit_time     timestamp default current_timestamp() not null on update current_timestamp(),
-    bill          varchar(255)                          not null,
-    constraint author_users_uname_fk
-        foreign key (author) references users (uname)
-            on delete cascade,
-    constraint notes_bills_id_fk
-        foreign key (bill) references bills (id)
+    biennium      varchar(255)                          not null,
+    bill_id       varchar(255)                          not null,
+    foreign key (author) references auth_user (id)
+        on delete cascade,
+    foreign key (biennium, bill_id) references bills(biennium, bill_id)
 );
-
