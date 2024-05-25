a0434c23e86cfd4ce920458b4ebb2334521e5f98
Steven Harper <20556807+stevenharperja@users.noreply.github.com>
2024-05-25 00:22:02 +0000

    add TODO

--- frontend/billorganizer_frontend/bill_app/components/bill_add.py
+++ frontend/billorganizer_frontend/bill_app/components/bill_add.py
@@ -5,6 +5,7 @@ from
 
 class ButtonAddView(UnicornView):
     already_clicked = False
-
+    
+    #TODO call util add bill to list function. and also integrate this view into the search bills view
     def add_bill(self):
         self.
