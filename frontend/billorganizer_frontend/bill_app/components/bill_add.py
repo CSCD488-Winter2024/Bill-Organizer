d531eb4d206318b812798ff83c9b9da17dc5ab15
Steven Harper <20556807+stevenharperja@users.noreply.github.com>
2024-05-26 00:29:13 +0000

    add utils import

--- frontend/billorganizer_frontend/bill_app/components/bill_add.py
+++ frontend/billorganizer_frontend/bill_app/components/bill_add.py
@@ -1,11 +1,17 @@
 from django_unicorn.components import UnicornView
 
-#import function from utils
-from 
+
+import sys
+import os
+
+from .. import utils
+
 
 class ButtonAddView(UnicornView):
     already_clicked = False
     
     #TODO call util add bill to list function. and also integrate this view into the search bills view
     def add_bill(self):
-        self.
+        
+        utils.get_default_list(user)
+        utils.mark_bill(list=,bill=)
