efd8d4cc1a095283511001a31a44ad1e73d7c520
Steven Harper <20556807+stevenharperja@users.noreply.github.com>
2024-05-26 01:17:07 +0000

    add function to pass variables to button?

--- frontend/billorganizer_frontend/bill_app/components/bill_add.py
+++ frontend/billorganizer_frontend/bill_app/components/bill_add.py
@@ -8,6 +8,12 @@ from .. import utils
 class BillAddView(UnicornView):
     already_clicked = False
     
+    def mount(self):
+        arg = self.component_args[0]
+        kwarg = self.component_kwargs["name"]
+
+        assert (f"{arg} {kwarg}" == "Hello World", kwarg)
+
     #TODO call util add bill to list function. and also integrate this view into the search bills view
     def add_bill(self,bill):
         user = get_user(self.request)
