30b89a8a244a44c34d3ab21003816316f4bcde5e
Steven Harper <20556807+stevenharperja@users.noreply.github.com>
2024-05-26 02:03:54 +0000

    fix syntax to use list id instead

--- frontend/billorganizer_frontend/bill_app/views.py
+++ frontend/billorganizer_frontend/bill_app/views.py
@@ -151,7 +151,7 @@ def mybills(request):
 
 
     #make a link to get list bills as excel
-    filepath = export.get_file(list)
+    filepath = export.get_file(list_id)
     http +="<a  href='{}' download> Download this list as CSV </a>".format(filepath) #TODO figure out when to delete the file afterward
 
 
