5b45b2669d9045969c77d2c4d8e7f8f55b2b4d89
Steven Harper <20556807+stevenharperja@users.noreply.github.com>
2024-05-27 00:27:01 +0000

    fix syntax

--- util.py
+++ util.py
@@ -24,7 +24,7 @@ def export(list_id: str) -> str:
             """, tuple(list_id))
 
         # csv.writer requires a file-like object, so we create a temporary file to hold the csv data
-        with tempfile.NamedTemporaryFile(mode='w+', delete=False, delete_on_close=False) as file:
+        with tempfile.NamedTemporaryFile(mode='w+', delete=False) as file:
             writer = csv.writer(file)
 
             # Grab the names of the bill table columns to use as headers
