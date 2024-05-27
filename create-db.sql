f5a46b43356502d5ce24fd41ede8dab5231df1b4
Gunnar Knutson <gunnar.knutson@proton.me>
2024-05-26 17:11:03 -0700

    update database schema, fix bill export, rename export file/function

--- create-db.sql
+++ create-db.sql
@@ -7,21 +7,17 @@ create table if not exists bills
     engrossed_version  tinyint unsigned                                          not null,
     original_agency    varchar(255)                                              not null,
     active             boolean                                                   not null,
-    companion_bill     varchar(255)                                              null,
     fiscal_notes       set('State', 'Local')                                     null,
-    requested_by       set('Governor', 'BudgetCommittee', 'Department', 'Other') null,
-    first_accessed     timestamp default CURRENT_TIMESTAMP                       not null,
-    last_accessed      timestamp default CURRENT_TIMESTAMP                       not null,
-    last_updated       timestamp default CURRENT_TIMESTAMP                       not null,
     appropriations     boolean                                                   not null,
+    requested_by       set('Governor', 'BudgetCommittee', 'Department', 'Other') null,
     short_description  varchar(255)                                              not null,
     request            varchar(255)                                              not null,
     introduced_date    timestamp                                                 not null,
-    sponsor            varchar(255)                                              not null,
     sponsor_id         smallint unsigned                                         not null,
     long_description   varchar(255)                                              not null,
     legal_title        varchar(255)                                              not null,
-    party              varchar(255)                                              not null,
+    companion_bill     varchar(255)                                              null,
+    last_updated       timestamp default CURRENT_TIMESTAMP                       not null,
     primary key (biennium, bill_id)
 );
 
@@ -31,25 +27,30 @@ create table if not exists status
     bill_id                  varchar(255)                        not null,
     history_line             varchar(255)                        not null,
     action_date              timestamp                           not null,
-    retrieved                timestamp default CURRENT_TIMESTAMP not null,
     amended_by_opposite_body boolean                             not null,
     vetoed                   set('true', 'partial', 'false')     not null,
     amendments_exist         boolean                             not null,
     status                   varchar(255)                        not null,
+    last_updated             timestamp default CURRENT_TIMESTAMP not null,
+    primary key (biennium, bill_id, action_date),
     foreign key (biennium, bill_id) references bills(biennium, bill_id)
 );
 
 create table if not exists sponsors
 (
-    biennium   varchar(255) not null,
-    party      varchar(255) not null,
-    district   varchar(255) not null,
-    phone      varchar(255) not null,
-    email      varchar(255) not null,
-    first_name varchar(255) not null,
-    last_name  varchar(255) not null,
-    primary key (biennium, email),
-    index (biennium, last_name)
+    biennium     varchar(255)                        not null,
+    id           int                                 not null,
+    long_name    varchar(255)                        not null,
+    agency       varchar(255)                        not null,
+    acronym      varchar(255)                        not null,
+    party        varchar(255)                        not null,
+    district     varchar(255)                        not null,
+    phone        varchar(255)                        not null,
+    email        varchar(255)                        not null,
+    first_name   varchar(255)                        not null,
+    last_name    varchar(255)                        not null,
+    last_updated timestamp default CURRENT_TIMESTAMP not null,
+    primary key (biennium, id)
 );
 
 create table if not exists lists
@@ -68,7 +69,8 @@ create table if not exists marks
     list uuid             not null,
     biennium varchar(255) not null,
     bill_id  varchar(255) not null,
-    foreign key (biennium, bill_id) references bills(biennium, bill_id),
+    foreign key (biennium, bill_id) references bills(biennium, bill_id)
+        on delete cascade,
     foreign key (list) references lists(id)
         on delete cascade
 );
@@ -86,4 +88,5 @@ create table if not exists notes
     foreign key (author) references auth_user (id)
         on delete cascade,
     foreign key (biennium, bill_id) references bills(biennium, bill_id)
+        on delete cascade
 );
--- util.py
+++ util.py
@@ -3,7 +3,7 @@ import tempfile
 import csv
 
 
-def get_file(list_id: str) -> str:
+def export(list_id: str) -> str:
     """
     Dumps the contents of the bills table into a csv-formatted file, and returns the file name.
     :param list_id: the uuid of the list to export. if NONE, export all bills.
@@ -11,9 +11,17 @@ def get_file(list_id: str) -> str:
     """
     with cfg.Cursor() as cur:
         if list_id is None:
-            cur.execute('SELECT * FROM bills')
+            cur.execute(r"""
+                select * from bills
+                    join sponsors on bills.biennium = sponsors.biennium and bills.sponsor_id = sponsors.id
+            """)
         else:
-            cur.execute('select bills.* from marks join bills on marks.bill_id = bills.bill_id where marks.list = ?', list_id)
+            cur.execute(r"""
+                select * from marks 
+                    join bills on marks.biennium = bills.biennium and marks.bill_id = bills.bill_id 
+                    join sponsors on bills.biennium = sponsors.biennium and bills.sponsor_id = sponsors.id 
+                    where marks.list = ?
+            """, tuple(list_id))
 
         # csv.writer requires a file-like object, so we create a temporary file to hold the csv data
         with tempfile.TemporaryFile(mode='w+', delete=False, delete_on_close=False) as file:
