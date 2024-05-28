18a3649c6d8bdae611f35e8b3f9ce6dc557e8c8a
Steven Harper <20556807+stevenharperja@users.noreply.github.com>
2024-05-28 05:44:55 +0000

    connect component to template

--- frontend/billorganizer_frontend/bill_app/views.py
+++ frontend/billorganizer_frontend/bill_app/views.py
@@ -190,9 +190,9 @@ def mybills(request):
 
 
 def bill_add(request): # see https://www.django-unicorn.com/docs/components/
-    # context = {"hello": {"world": {"name": "Galaxy"}}}
+    context = {}#{"hello": {"world": {"name": "Galaxy"}}}
 
-    return render(request, "unicorn/bill_add.html") #, context=context)
+    return render(request, "unicorn/bill_add.html", context=context)
     # template = loader.get_template('master.html')
     #   return HttpResponse(template.render())
     # return loader.get_template("unicorn/bill_add.html").render() #, context=context)
