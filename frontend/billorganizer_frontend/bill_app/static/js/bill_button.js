3f9563238bcd43d3023a531ae974f46a2c5912c5
shweshipu <shwesh2>
2024-06-04 22:41:19 -0700

    refactor and move javascript to another file

--- frontend/billorganizer_frontend/bill_app/static/js/bill_button.js
+++ frontend/billorganizer_frontend/bill_app/static/js/bill_button.js
@@ -20,3 +20,31 @@ async function add_all() {
         });
         console.log(response.data);
   }
+
+
+  const csrftoken = document.querySelector('[name=csrfmiddlewaretoken]').value
+
+  async function handleNotes(num, bill_biennium, bill_bill_id) {
+      const box = document.getElementById("notebox-" + num);
+
+      if (box.hidden === true) {
+          const note = await axios.get('/getnote/' + bill_biennium + '/' + bill_bill_id)
+          console.log(note);
+          document.getElementById(num + '-notelabel').value = note.data
+          box.hidden = false
+          document.getElementById(num + "-notebutton").textContent = "save"
+      }
+      else {
+          document.getElementById(num + "-notebutton").textContent = "notes"
+          const text = document.getElementById(num + '-notelabel').value
+          console.log(text);
+          await axios.put(`/writenote/${bill_biennium}/${bill_bill_id}/`, {
+              text
+          },
+          {
+              headers: {'X-CSRFToken': csrftoken}
+          })
+          box.hidden = true
+          document.getElementById(num + "-notebutton").textContent = "note"
+      }
+  }
